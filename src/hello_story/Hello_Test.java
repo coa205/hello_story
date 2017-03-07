caee0b3b6b7980b6ad82396a41197acfc8ec30c5
coa205 <coa205@naver.com>
2017-03-07 10:50:34 +0900

    tag ver02 테스트

--- src/hello_story/Hello_Test.java
+++ src/hello_story/Hello_Test.java
@@ -7,5 +7,8 @@ public class Hello_Test {
 		System.out.println("로컬에서 추가");
 		System.out.println("Git Hub Edit");
 		System.out.println("last add");
+		
+		///////////////
+		System.out.println("Ver02 테스트");
 	}
 }
